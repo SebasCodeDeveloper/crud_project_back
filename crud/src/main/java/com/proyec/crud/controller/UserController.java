9714a57b442ad9a85e34c54e626cf24dc33f5dc8
SebasCodeDeveloper <sebatianpena950@gmail.com>
2025-11-19 19:23:22 -0500

    Implementacion de metodos GetUse, GetAll, UpdateUser

--- crud/src/main/java/com/proyec/crud/controller/UserController.java
+++ crud/src/main/java/com/proyec/crud/controller/UserController.java
@@ -1,31 +1,49 @@
 package com.proyec.crud.controller;
 
 import com.proyec.crud.entity.User;
+import com.proyec.crud.model.UserRq;
 import com.proyec.crud.model.UserRs;
 import com.proyec.crud.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-    @RequestMapping("/api/users")
+@RequestMapping("/api/users")
 
 public class UserController {
 
     private final UserService service;
 
 @PostMapping
-    public ResponseEntity<UserRs> create(@RequestBody User user) {
-        return ResponseEntity.ok(service.createUser(user));
+    public ResponseEntity<UserRs> create(@RequestBody UserRq request) {
+        return ResponseEntity.ok(service.createUser(request));
+    }
+
+@GetMapping("/{id}")
+    public ResponseEntity<UserRs> GetUser(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.GetUser(id));
     }
 
 @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable UUID id) {
     service.deleteUser(id);
         return ResponseEntity.ok("Usuario eliminado correctamente");
+    }
 
+@GetMapping()
+    public ResponseEntity<List<UserRs>>GetAll() {
+        return ResponseEntity.ok(service.GetAll());
+    }
+
+
+@PutMapping ("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable UUID id, @RequestBody UserRq request ) {
+        service.updateUser(id, request);
+        return ResponseEntity.ok(service.updateUser(id, request));
     }
 }
--- crud/src/main/java/com/proyec/crud/service/impl/UserServiceImpl.java
+++ crud/src/main/java/com/proyec/crud/service/impl/UserServiceImpl.java
@@ -1,44 +1,87 @@
 package com.proyec.crud.service.impl;
 
 import com.proyec.crud.entity.User;
+import com.proyec.crud.model.UserRq;
 import com.proyec.crud.model.UserRs;
 import com.proyec.crud.repository.UserRepository;
 import com.proyec.crud.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository repository;
+    private final UserRepository UserRepository;
 
     @Override
-    public UserRs createUser(User user) {
+    public UserRs createUser(UserRq request) {
 
         // Validar si el email ya existe
-        repository.findByEmail(user.getEmail())
+        UserRepository.findByEmail(request.getEmail())
                 .ifPresent(u -> {
                     throw new RuntimeException("El email ya está registrado");
                 });
 
+User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
         // Guardar usuario
-        User userSaved = repository.save(user);
+        User userSaved = UserRepository.save(user);
 
         // Devolver respuesta sin contraseña
         return toRs(userSaved);
     }
 
+    @Override
+    public UserRs GetUser(UUID id) {
+        User user = UserRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("El usuario no existe"));
 
+        return toRs(user);
+    }
+
+    @Override
+    public List<UserRs> GetAll() {
+        return UserRepository.findAll().stream().map(this::toRs).collect(Collectors.toList());
+    }
 
     @Override
     public void deleteUser(UUID id) {
-        User user = repository.findById(id)
+        User user = UserRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("El usuario no existe"));
-        repository.delete(user);
+        UserRepository.delete(user);
         new UserRs("Usuario eliminado", user.getName());
+    }
+
+
+    @Override
+    public UserRs updateUser(UUID id, UserRq request) {
+        // Buscar el usuario
+        User user = UserRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("El usuario no existe"));
+
+        // Validar que el nuevo email no esté usado por otro usuario
+        UserRepository.findByEmail(request.getEmail())
+                .filter(u -> !u.getId().equals(id))
+                .ifPresent(u -> {
+                    throw new RuntimeException("El email ya esta registrado por otro usuario");
+                });
+
+        //Actualizar Datos
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        User userUpdate = UserRepository.save(user);
+
+        // Devolver respuesta sin contraseña
+        return toRs(userUpdate);
     }
 
     private UserRs toRs(User user){
@@ -50,6 +93,8 @@ public class UserServiceImpl implements UserService {
         return rs;
 
     }
+
+
 }
 
 
